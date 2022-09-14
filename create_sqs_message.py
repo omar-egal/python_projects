3c590c170113a4f35436c87db705358778d3ef5c
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-09-14 20:54:02 +0000

    edited comment

--- create_sqs_message.py
+++ create_sqs_message.py
@@ -8,7 +8,6 @@ response = "The time and date is: "
 date_time = datetime.now()
 
 #send sqs message with the current date & time
-
 message = client.send_message(
     QueueUrl='https://queue.amazonaws.com/191325089225/week15-sqs-queue',
     MessageBody=("This was processed on: " + str(date_time.strftime('%Y-%m-%d %H:%M:%S')))
